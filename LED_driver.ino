0c19ba278a9b0ccf5c0f5a93cf4d1e1e7956a5c5
Graeme Bragg <gmb1g08@ecs.soton.ac.uk>
2016-09-05 16:46:39 +0000

    LED Count is stored in EEPROM so one controller *could* be used for either dome.
    Init string depends on configured number of LEDs
    
    Split Autorun LED order off into different files - one for each dome-type.
    
    Transitioned a lot of variables to #defines
    
    git-svn-id: https://svn.glacsweb.info/rti/LED_Driver_PCB/LED_driver@305 849e05f9-b289-0410-b515-aa01ba9062ae

--- LED_driver.ino
+++ LED_driver.ino
@@ -7,11 +7,21 @@
  WARNING: All arrays are bytes so sizeof = length
 */
 
-#define DEBUG                   1
+/* ----------------------------------- Compile-time settings ---------------------------------- */  
+#define DEBUG                   1     // Define whether to output debug info on debug serial port
+#define HAS_SCREEN              1     // Define whether a serial screen is connected to Serial2
+#define LED_BURN_CHECK          1     // Define whether to enable LED_BURN_CHECK
+#define BUTTONS                 5     // The number of buttons connected to the controller
+#define DEFAULT_NUM_LEDS        128   // The default number of LEDs connected. Currently only 76 and 128 are supported.
 
-#define MAX_LEDS                128
+#define OVERWRITE_NUM_LEDS      128   // Compile-time overwite value for num_leds. This should be set, flashed, commented out and then re-flashed.
 
-#define HAS_SCREEN              1
+/* -------------------------------------------------------------------------------------------- */
+
+/* -------------------------------- System Config Definitions --------------------------------- */  
+// EEPROM Config
+#include <EEPROM.h>
+#define ADDR_NUM_LEDS           0
 
 // Serial Port Assignments
 #define DEBUG_SERIAL            Serial
@@ -19,6 +29,7 @@
 #define CONSOLE                 Serial3
 
 //OUTPUT BANKS
+#define MAX_LEDS                128
 #define LED_BANKS               3
 #define A                       0
 #define B                       1
@@ -30,32 +41,76 @@
 #define TRIGGER                 39 // Package pin 70: Input to start automated capture
 #define AUTOMATED_RUNNING_LED   40 // Package pin 52
 
-#define BUTTONS                 5 // The number of buttons connected to the controller
+//Button Config
 #define BUTTON_0
 #define BUTTON_1
 #define BUTTON_2
 #define BUTTON_3
 #define BUTTON_4
 
-//LED OUTPUTS
+// Default LED setting sanity check
+#if ((DEFAULT_NUM_LEDS != 76) & (DEFAULT_NUM_LEDS != 128))
+  #error "Unsupported Default Dome Size."
+#endif
+
+//LED Variables
+uint8_t num_leds;
 byte leds[LED_BANKS][8];
+byte AUTORUN_LEDS[MAX_LEDS][LED_BANKS];
 
 //Automated running
 int LIGHT_ON_TIME = 1500;
 int PRE_ON_DELAY = 100;
 int SHUTTER_ACTUATION_TIME = 100;
 int BETWEEN_SHOT_DELAY = 100;
-byte AUTORUN_LEDS[MAX_LEDS][LED_BANKS];
+/* -------------------------------------------------------------------------------------------- */
 
-//enable watchdog checks
-int ledburncheck = 1;
   
 void debug(String output){
   //CONSOLE.println(output);
 }
 
 void setup() {
+/* ------------------------------------ Setup serial ports ------------------------------------ */  
+  CONSOLE.begin(38400); //init serial port
+  CONSOLE.setTimeout(100);
 
+#if DEBUG
+  DEBUG_SERIAL.begin(9600);
+  DEBUG_SERIAL.setTimeout(100);
+#endif
+
+#if HAS_SCREEN
+  SCREEN.begin(9600); //init serial port
+  SCREEN.setTimeout(100);
+#endif
+/* -------------------------------------------------------------------------------------------- */
+
+/* --------------------------- Check & update stored number of LEDs --------------------------- */
+  num_leds = EEPROM.read(ADDR_NUM_LEDS);    // Get the number of LEDs stored in EEPROM
+
+#ifdef OVERWRITE_NUM_LEDS
+  #warning Compile-Time overwrite set for LED Number. Please undef OVERWRITE_NUM_LEDS and reflash
+#if ((OVERWRITE_NUM_LEDS != 76) & (OVERWRITE_NUM_LEDS != 128))
+  #error "Unsupported Overwrite Dome Size."
+#endif  
+  num_leds = OVERWRITE_NUM_LEDS;
+  EEPROM.put(ADDR_NUM_LEDS, OVERWRITE_NUM_LEDS);
+#endif
+
+  if((num_leds != 76) && (num_leds != 128)) {
+    // Number of LEDs stored in EEPROM is corrupt. Reset to default.
+#if DEBUG
+    DEBUG_SERIAL.write("Error reading Number of LEDs from EEPROM: defaulting to ");
+    DEBUG_SERIAL.print(DEFAULT_NUM_LEDS, DEC);
+    DEBUG_SERIAL.write(" LEDs\r\n");
+#endif    
+    num_leds = DEFAULT_NUM_LEDS;
+    EEPROM.put(ADDR_NUM_LEDS, DEFAULT_NUM_LEDS);
+  }
+/* -------------------------------------------------------------------------------------------- */
+
+/* ------------------------------- Set LED Row and Column pins -------------------------------- */ 
   /* Rows 0-7 (+28V) */
   leds[A][0] = 22; // Package Pin 78
   leds[A][1] = 23; // Package Pin 77
@@ -85,272 +140,56 @@ void setup() {
   leds[C][5] = 44;	// Package Pin 40
   leds[C][6] = 43;	// Package Pin 41
   leds[C][7] = 42; 	// Package Pin 42
-  
-// Quarter A
-  AUTORUN_LEDS[0][0] = 1; //1
-  AUTORUN_LEDS[0][1] = 8;
-  AUTORUN_LEDS[0][2] = 0; 
-  AUTORUN_LEDS[1][0] = 1; //2
-  AUTORUN_LEDS[1][1] = 128;
-  AUTORUN_LEDS[1][2] = 0;
-  AUTORUN_LEDS[2][0] = 2; //3
-  AUTORUN_LEDS[2][1] = 128;
-  AUTORUN_LEDS[2][2] = 0;
-  AUTORUN_LEDS[3][0] = 4;  //4
-  AUTORUN_LEDS[3][1] = 128;
-  AUTORUN_LEDS[3][2] = 0;
-  AUTORUN_LEDS[4][0] = 8;  //5
-  AUTORUN_LEDS[4][1] = 128;
-  AUTORUN_LEDS[4][2] = 0;
-  AUTORUN_LEDS[5][0] = 1;  //6
-  AUTORUN_LEDS[5][1] = 16;
-  AUTORUN_LEDS[5][2] = 0;
-  AUTORUN_LEDS[6][0] = 1;  //7
-  AUTORUN_LEDS[6][1] = 64;
-  AUTORUN_LEDS[6][2] = 0;
-  AUTORUN_LEDS[7][0] = 2;  //8
-  AUTORUN_LEDS[7][1] = 64;
-  AUTORUN_LEDS[7][2] = 0;
-  AUTORUN_LEDS[8][0] = 4;  //9
-  AUTORUN_LEDS[8][1] = 64;  
-  AUTORUN_LEDS[8][2] = 0;
-  AUTORUN_LEDS[9][0] = 8;  //10  
-  AUTORUN_LEDS[9][1] = 64;
-  AUTORUN_LEDS[9][2] = 0;  
-  AUTORUN_LEDS[10][0] = 1;  //11
-  AUTORUN_LEDS[10][1] = 32;  
-  AUTORUN_LEDS[10][2] = 0;
-  AUTORUN_LEDS[11][0] = 2;  //12
-  AUTORUN_LEDS[11][1] = 32;
-  AUTORUN_LEDS[11][2] = 0;  
-  AUTORUN_LEDS[12][0] = 4;  //13
-  AUTORUN_LEDS[12][1] = 32;  
-  AUTORUN_LEDS[12][2] = 0;
-  AUTORUN_LEDS[13][0] = 8;  //14
-  AUTORUN_LEDS[13][1] = 32;
-  AUTORUN_LEDS[13][2] = 0;  
-  AUTORUN_LEDS[14][0] = 2;  //15
-  AUTORUN_LEDS[14][1] = 16; 
-  AUTORUN_LEDS[14][2] = 0;
-  AUTORUN_LEDS[15][0] = 4;  //16
-  AUTORUN_LEDS[15][1] = 16;
-  AUTORUN_LEDS[15][2] = 0;
-  AUTORUN_LEDS[16][0] = 8;  //17
-  AUTORUN_LEDS[16][1] = 16;
-  AUTORUN_LEDS[16][2] = 0;
-  AUTORUN_LEDS[17][0] = 4;  //18
-  AUTORUN_LEDS[17][1] = 8;
-  AUTORUN_LEDS[17][2] = 0;
-  AUTORUN_LEDS[18][0] = 8;   //19
-  AUTORUN_LEDS[18][1] = 8;
-  AUTORUN_LEDS[18][2] = 0;
 
-//Quarter B
-  AUTORUN_LEDS[19][0] = 16;  //1
-  AUTORUN_LEDS[19][1] = 8;
-  AUTORUN_LEDS[19][2] = 0;
-  AUTORUN_LEDS[20][0] = 16;  //2
-  AUTORUN_LEDS[20][1] = 128;
-  AUTORUN_LEDS[20][2] = 0;
-  AUTORUN_LEDS[21][0] = 32;  //3
-  AUTORUN_LEDS[21][1] = 128;
-  AUTORUN_LEDS[21][2] = 0;
-  AUTORUN_LEDS[22][0] = 64;  //4
-  AUTORUN_LEDS[22][1] = 128;
-  AUTORUN_LEDS[22][2] = 0;
-  AUTORUN_LEDS[23][0] = 128;  //5
-  AUTORUN_LEDS[23][1] = 128;
-  AUTORUN_LEDS[23][2] = 0;
-  AUTORUN_LEDS[24][0] = 16;  //6
-  AUTORUN_LEDS[24][1] = 16;
-  AUTORUN_LEDS[24][2] = 0;
-  AUTORUN_LEDS[25][0] = 16;  //7
-  AUTORUN_LEDS[25][1] = 64;
-  AUTORUN_LEDS[25][2] = 0;
-  AUTORUN_LEDS[26][0] = 32;  //8
-  AUTORUN_LEDS[26][1] = 64;
-  AUTORUN_LEDS[26][2] = 0;
-  AUTORUN_LEDS[27][0] = 64;  //9
-  AUTORUN_LEDS[27][1] = 64;
-  AUTORUN_LEDS[27][2] = 0;
-  AUTORUN_LEDS[28][0] = 128;  //10
-  AUTORUN_LEDS[28][1] = 64;
-  AUTORUN_LEDS[28][2] = 0;
-  AUTORUN_LEDS[29][0] = 16;  //11
-  AUTORUN_LEDS[29][1] = 32;
-  AUTORUN_LEDS[29][2] = 0;
-  AUTORUN_LEDS[30][0] = 32;  //12
-  AUTORUN_LEDS[30][1] = 32;
-  AUTORUN_LEDS[30][2] = 0;
-  AUTORUN_LEDS[31][0] = 64;  //13
-  AUTORUN_LEDS[31][1] = 32;
-  AUTORUN_LEDS[31][2] = 0;
-  AUTORUN_LEDS[32][0] = 128;  //14
-  AUTORUN_LEDS[32][1] = 32;
-  AUTORUN_LEDS[32][2] = 0;
-  AUTORUN_LEDS[33][0] = 32;  //15
-  AUTORUN_LEDS[33][1] = 16;
-  AUTORUN_LEDS[33][2] = 0;
-  AUTORUN_LEDS[34][0] = 64;  //16
-  AUTORUN_LEDS[34][1] = 16;
-  AUTORUN_LEDS[34][2] = 0;
-  AUTORUN_LEDS[35][0] = 128;  //17
-  AUTORUN_LEDS[35][1] = 16;
-  AUTORUN_LEDS[35][2] = 0;
-  AUTORUN_LEDS[36][0] = 64;  //18
-  AUTORUN_LEDS[36][1] = 8;
-  AUTORUN_LEDS[36][2] = 0;
-  AUTORUN_LEDS[37][0] = 128;  //19
-  AUTORUN_LEDS[37][1] = 8;
-  AUTORUN_LEDS[37][2] = 0;
+/* -------------------------------------------------------------------------------------------- */
 
-//Quarter C
-  AUTORUN_LEDS[38][0] = 0;  //1
-  AUTORUN_LEDS[38][1]= 8;
-  AUTORUN_LEDS[38][2] = 1;
-  AUTORUN_LEDS[39][0] = 0;  //2
-  AUTORUN_LEDS[39][1] = 128;
-  AUTORUN_LEDS[39][2] = 1;
-  AUTORUN_LEDS[40][0] = 0;  //3
-  AUTORUN_LEDS[40][1] = 128;
-  AUTORUN_LEDS[40][2] = 2;
-  AUTORUN_LEDS[41][0] = 0;  //4
-  AUTORUN_LEDS[41][1] = 128;
-  AUTORUN_LEDS[41][2] = 4;
-  AUTORUN_LEDS[42][0] = 0;  //5
-  AUTORUN_LEDS[42][1] = 128;
-  AUTORUN_LEDS[42][2] = 8;
-  AUTORUN_LEDS[43][0] = 0;  //6
-  AUTORUN_LEDS[43][1] = 16;
-  AUTORUN_LEDS[43][2] = 1;
-  AUTORUN_LEDS[44][0] = 0;  //7
-  AUTORUN_LEDS[44][1] = 64;
-  AUTORUN_LEDS[44][2] = 1;
-  AUTORUN_LEDS[45][0] = 0;  //8
-  AUTORUN_LEDS[45][1] = 64;
-  AUTORUN_LEDS[45][2] = 2;
-  AUTORUN_LEDS[46][0] = 0;  //9
-  AUTORUN_LEDS[46][1] = 64;
-  AUTORUN_LEDS[46][2] = 4;
-  AUTORUN_LEDS[47][0] = 0;  //10
-  AUTORUN_LEDS[47][1] = 64;
-  AUTORUN_LEDS[47][2] = 8;
-  AUTORUN_LEDS[48][0] = 0;  //11
-  AUTORUN_LEDS[48][1] = 32;
-  AUTORUN_LEDS[48][2] = 1;
-  AUTORUN_LEDS[59][0] = 0;  //12
-  AUTORUN_LEDS[49][1] = 32;
-  AUTORUN_LEDS[49][2] = 2;
-  AUTORUN_LEDS[50][0] = 0;  //13
-  AUTORUN_LEDS[50][1] = 32;
-  AUTORUN_LEDS[50][2] = 4;
-  AUTORUN_LEDS[51][0] = 0;  //14
-  AUTORUN_LEDS[51][1] = 32;
-  AUTORUN_LEDS[51][2] = 8;
-  AUTORUN_LEDS[52][0] = 0;   //15
-  AUTORUN_LEDS[52][1] = 16;
-  AUTORUN_LEDS[52][2] = 2;
-  AUTORUN_LEDS[53][0] = 0;  //16
-  AUTORUN_LEDS[53][1] = 16;
-  AUTORUN_LEDS[53][2] = 4;
-  AUTORUN_LEDS[54][0] = 0;  //17
-  AUTORUN_LEDS[54][1] = 16;
-  AUTORUN_LEDS[54][2] = 8;
-  AUTORUN_LEDS[55][0] = 0;  //18
-  AUTORUN_LEDS[55][1] = 8;
-  AUTORUN_LEDS[55][2] = 4;
-  AUTORUN_LEDS[56][0] = 0;  //19
-  AUTORUN_LEDS[56][1] = 8;
-  AUTORUN_LEDS[56][2] = 8;
+/* ------------------------------- Write initialisation strings ------------------------------- */ 
+#if HAS_SCREEN
+  SCREEN.write(254);
+  SCREEN.write(128);
+  SCREEN.write("RTI ");
+#endif
 
-//Quarter D
-  AUTORUN_LEDS[57][0] = 0;  //1
-  AUTORUN_LEDS[57][1] = 8;
-  AUTORUN_LEDS[57][2] = 16;
-  AUTORUN_LEDS[58][0] = 0;  //2
-  AUTORUN_LEDS[58][1] = 128;
-  AUTORUN_LEDS[58][2] = 16;
-  AUTORUN_LEDS[59][0] = 0;  //3
-  AUTORUN_LEDS[59][1] = 128;
-  AUTORUN_LEDS[59][2] = 32;
-  AUTORUN_LEDS[60][0] = 0;  //4
-  AUTORUN_LEDS[60][1] = 128;
-  AUTORUN_LEDS[60][2] = 64;
-  AUTORUN_LEDS[61][0] = 0;  //5
-  AUTORUN_LEDS[61][1] = 128;
-  AUTORUN_LEDS[61][2] = 128;
-  AUTORUN_LEDS[62][0] = 0;  //6
-  AUTORUN_LEDS[62][1] = 16;
-  AUTORUN_LEDS[62][2] = 16;
-  AUTORUN_LEDS[63][0] = 0;  //7
-  AUTORUN_LEDS[63][1] = 64;
-  AUTORUN_LEDS[63][2] = 16;
-  AUTORUN_LEDS[64][0] = 0;  //8
-  AUTORUN_LEDS[64][1] = 64;
-  AUTORUN_LEDS[64][2] = 32;
-  AUTORUN_LEDS[65][0] = 0;  //9
-  AUTORUN_LEDS[65][1] = 64;
-  AUTORUN_LEDS[65][2] = 64;
-  AUTORUN_LEDS[66][0] = 0;  //10
-  AUTORUN_LEDS[66][1] = 64;
-  AUTORUN_LEDS[66][2] = 128;
-  AUTORUN_LEDS[67][0] = 0;   //11
-  AUTORUN_LEDS[67][1] = 32;
-  AUTORUN_LEDS[67][2] = 16;
-  AUTORUN_LEDS[68][0] = 0;  //12
-  AUTORUN_LEDS[68][1] = 32;
-  AUTORUN_LEDS[68][2] = 32;
-  AUTORUN_LEDS[69][0] = 0;  //13
-  AUTORUN_LEDS[69][1] = 32;
-  AUTORUN_LEDS[69][2] = 64;
-  AUTORUN_LEDS[70][0] = 0;  //14
-  AUTORUN_LEDS[70][1] = 32;
-  AUTORUN_LEDS[70][2] = 128;
-  AUTORUN_LEDS[71][0] = 0;  //15
-  AUTORUN_LEDS[71][1] = 16;
-  AUTORUN_LEDS[71][2] = 32;
-  AUTORUN_LEDS[72][0] = 0;  //16
-  AUTORUN_LEDS[72][1] = 16;
-  AUTORUN_LEDS[72][2] = 64;
-  AUTORUN_LEDS[73][0] = 0;  //17
-  AUTORUN_LEDS[73][1] = 16;
-  AUTORUN_LEDS[73][2] = 128;
-  AUTORUN_LEDS[74][0] = 0;  //17
-  AUTORUN_LEDS[74][1] = 8;
-  AUTORUN_LEDS[74][2] = 64;
-  AUTORUN_LEDS[75][0] = 0;  //19
-  AUTORUN_LEDS[75][1] = 8;
-  AUTORUN_LEDS[75][2] = 128;
-  
-  CONSOLE.begin(38400); //init serial port
-  CONSOLE.setTimeout(100);
+if(num_leds == 76) {
+  // Standard 76-LED Dome
   CONSOLE.write("RTI DOME Controller v0.2 \r\n");
 
 #if DEBUG
-  DEBUG_SERIAL.begin(9600);
-  DEBUG_SERIAL.setTimeout(100);
   DEBUG_SERIAL.write("RTI DOME Controller v0.2 \r\n");
 #endif
 
-
 #if HAS_SCREEN
-  SCREEN.begin(9600); //init serial port
-  SCREEN.setTimeout(100);
-  SCREEN.write(254);
-  SCREEN.write(128);
-  SCREEN.write("                ");
-  SCREEN.write(254);
-  SCREEN.write(192);
-  SCREEN.write("                ");
-  SCREEN.write(254);
-  SCREEN.write(128);
-  SCREEN.write("RTI DOME");
-  SCREEN.write(254);
-  SCREEN.write(192);
-  SCREEN.write("Controller v0.2");
+  SCREEN.write("DOME        ");
+#endif
+} else if(num_leds ==128) {
+  // 128-LED SuperDome
+  CONSOLE.write("RTI SUPERDOME Controller v0.2 \r\n");
+
+#if DEBUG
+  DEBUG_SERIAL.write("RTI SUPERDOME Controller v0.2 \r\n");
 #endif
 
-  //Setup IO
+#if HAS_SCREEN
+  SCREEN.write("SUPERDOME   ");
+#endif
+}
+
+#if HAS_SCREEN  
+  SCREEN.write(254);
+  SCREEN.write(192);
+  SCREEN.write("Controller v0.2 ");
+#endif
+/* -------------------------------------------------------------------------------------------- */
+
+/* -------------------------------- Setup autorun LED sequence -------------------------------- */
+  if(num_leds == 76)  {  
+    setup_autorun_dome();
+  } else if (num_leds == 128) {
+    setup_autorun_superdome();
+  }
+/* -------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------- Setup I/O pins -------------------------------------- */ 
   pinMode(TRIGGER, INPUT);
   pinMode(CAMERA_SHUTTER, OUTPUT); 
   pinMode(AUTOMATED_RUNNING_LED, OUTPUT);
@@ -361,9 +200,19 @@ void setup() {
         pinMode(leds[i][j], OUTPUT); //set the pin for the LED as an output
       }
   }
+/* -------------------------------------------------------------------------------------------- */
+
+/* ------------------------------------ Initialise Watchdog ----------------------------------- */
   watchdoginit();
-  
+/* -------------------------------------------------------------------------------------------- */
+
+/* -------------------------------------- Setup I/O pins -------------------------------------- */   
   CONSOLE.write("Init Complete\r\n");
+  
+#if DEBUG
+    DEBUG_SERIAL.write("Init Complete\r\n");
+#endif 
+/* -------------------------------------------------------------------------------------------- */  
 }
 
 boolean multiple_leds(byte input){
@@ -390,14 +239,18 @@ void watchdoginit() {
  }
  
 void watchdogstop() {
-  debug("dog stop");
+#if DEBUG
+  DEBUG_SERIAL.write("\tWatchdog STOP\r\n");
+#endif
   //digitalWrite(DEBUG_LED, LOW);
   // Set CS12 and CS10 bits ZERO to stop timer 
   TCCR1B &= !((1 << CS12) | (1 << CS10)); 
 }
 
 void watchdogstart() {
-  debug("dog start");
+#if DEBUG
+  DEBUG_SERIAL.write("\tWatchdog START\r\n");
+#endif
   digitalWrite(DEBUG_LED, HIGH);
   TCNT1  = 0;//initialize counter value to 0
   // Set CS12 and CS10 bits for 1024 prescaler and start
@@ -467,9 +320,9 @@ void autorun(){
   SCREEN.write(254);
   SCREEN.write(192);
   SCREEN.write("    /");
-  SCREEN.print(75, DEC);
+  SCREEN.print(num_leds, DEC);
   
-  for(int i = 0; i < 76; i++){
+  for(int i = 0; i < num_leds; i++){
       SCREEN.write(254);
       SCREEN.write(192);
       SCREEN.write("    ");
@@ -552,7 +405,7 @@ void process(byte bank, byte state_in){
 
 ISR(TIMER1_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
 //generates pulse wave of frequency 2kHz/2 = 1kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (ledburncheck){
+  if (LED_BURN_CHECK){
    	process(A,char(0));
   	process(B,char(0));
 	  process(C,char(0));
